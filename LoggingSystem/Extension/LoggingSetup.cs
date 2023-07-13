8e5cbc35e51706bb60d184820012efb15f7d36dd
Eduardo Bagarrão <general@eduardobagarrao.com>
2023-07-13 22:34:40 +0100

    - Removed commentary

--- LoggingSystem/Extension/LoggingSetup.cs
+++ LoggingSystem/Extension/LoggingSetup.cs
@@ -8,7 +8,6 @@ namespace LoggingSystem.Extension
         public static IHostBuilder ConfigureLogging(this WebApplicationBuilder builder)
         {
             return builder.Host.UseSerilog(new LoggerConfiguration()
-                   //.WriteTo.Seq("http://localhost:5341")
                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
