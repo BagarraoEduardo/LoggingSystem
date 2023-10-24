e961f2543fcccb2d6d1be1917b5c860cdfcfc5ef
Eduardo Bagarrão <general@eduardobagarrao.com>
2023-10-25 00:10:40 +0100

    Configured Seq for MacOS

--- LoggingSystem/Extension/LoggingSetup.cs
+++ LoggingSystem/Extension/LoggingSetup.cs
@@ -7,12 +7,15 @@ namespace LoggingSystem.Extension
     {
         public static IHostBuilder ConfigureLogging(this WebApplicationBuilder builder)
         {
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+            
             return builder.Host.UseSerilog(new LoggerConfiguration()
                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
                    .ReadFrom.Configuration(builder.Configuration)
                    .CreateLogger());
+            
         }
     }
 }
